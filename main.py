76b8cc98284adccef9870b98d8446f0839e62dda
jxnsol1205 <s230207@baeksin.ms.kr>
2024-07-24 12:28:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import koreanize_matplotlib
 
 # 파일 업로드
-st.title("중학생 연령대 인구 비율")
+st.title("고등학생 연령대 인구 비율")
 uploaded_file = st.file_uploader("CSV 파일을 업로드하세요:", type="csv")
 
 if uploaded_file is not None:
@@ -12,6 +12,7 @@ if uploaded_file is not None:
 
     # 중학생 연령대 추출
     middle_school_ages = ['2024년06월_계_17세', '2024년06월_계_18세', '2024년06월_계_19세']
+
 
     # 지역 선택
     selected_region = st.selectbox('지역을 선택하세요:', data['행정구역'].unique())
@@ -29,7 +30,7 @@ if uploaded_file is not None:
     middle_school_ratio = (middle_school_population / total_population) * 100
 
     # 원 그래프 생성
-    labels = ['중학생 연령대', '기타 연령대']
+    labels = ['고등학생 연령대', '기타 연령대']
     sizes = [middle_school_ratio, 100 - middle_school_ratio]
     colors = ['#ff9999', '#66b3ff']
     explode = (0.1, 0)
